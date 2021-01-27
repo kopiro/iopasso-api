e56e7c5a15b02d4cf5db490b44f13cdd65028dd7
Flavio De Stefano <destefano.flavio@gmail.com>
2021-01-27 19:40:49 +0100

    Adding register and login

--- src/main.py
+++ src/main.py
@@ -4,12 +4,16 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms import Form, StringField, PasswordField, validators
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 class LoginForm(Form):
     email = StringField("email", validators=[validators.Length(
         min=7, max=50), validators.DataRequired(message="Email is required")])
     password = PasswordField("password", validators=[
                              validators.DataRequired(message="Password is required")])
+
 
 class RegisterForm(Form):
     name = StringField("name", validators=[validators.Length(
@@ -31,45 +35,64 @@ db = SQLAlchemy()
 app = Flask(__name__)
 CORS(app)
 
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/db.sqlite'
+db_filename = 'sqlite:///' + os.getcwd() + '/data/db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_filename
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    __tablename__ = 'usertable'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(15), unique=True)
+    name = db.Column(db.String(15))
     email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(256), unique=True)
-    birth_date = db.Column(db.String(10), unique=True)
+    password = db.Column(db.String(256))
+    birth_date = db.Column(db.String(10))
 
 
 @app.route('/register', methods=['POST'])
 def register():
     form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        hashed_password = generate_password_hash(
-            form.password.data, method='sha256')
-        new_user = User(
-            name=form.name.data,
-            email=form.email.data,
-            birth_date=form.birth_date.data,
-            password=hashed_password)
+    if form.validate() == False:
+        return jsonify({'error': True, 'message': "Validation failed", 'validations': list(form.errors.items())}), 400
 
-        db.session.add(new_user)
-        db.session.commit()
+    existing_user = User.query.filter_by(email=form.email.data).first()
+    if existing_user:
+        return jsonify({'error': True, 'message': 'This user already exists'}), 400
 
-        return jsonify({'success': True})
-    else:
-        return jsonify({'error':True,'validations':list(form.errors.items())})
+    hashed_password = generate_password_hash(
+        form.password.data, method='sha256')
+    user = User(
+        name=form.name.data,
+        email=form.email.data,
+        birth_date=form.birth_date.data,
+        password=hashed_password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'success': True}), 200
 
 
-if __name__ == '__main__':
-    # Creating database tables
+@app.route('/login', methods=['POST'])
+def login():
+    form = LoginForm(request.form)
+    if form.validate() == False:
+        return jsonify({'error': True, 'message': 'Validation failed', 'validations': list(form.errors.items())}), 400
+
+    user = User.query.filter_by(email=form.email.data).first()
+    if not user:
+        return jsonify({'error': True, 'message': 'Bad credentials, email or password'}), 401
+
+    if not check_password_hash(user.password, form.password.data):
+        return jsonify({'error': True, 'message': 'Bad credentials, email or password'}), 401
+
+    return jsonify({'id': user.id}), 200
+
+
+if __name__ == 'main':
     db.create_all()
-    # running server
     app.run(debug=True)
