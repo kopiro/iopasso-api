2726341702be532578567169a19139c0a1a590bd
Flavio De Stefano <destefano.flavio@gmail.com>
2021-01-29 20:17:16 +0100

    adding JWT and /guest and /me

--- src/main.py
+++ src/main.py
@@ -5,6 +5,11 @@ from wtforms import Form, StringField, PasswordField, validators
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import (create_access_token, create_refresh_token,
+                                jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
+from sqlalchemy_serializer import SerializerMixin
+
 load_dotenv()
 
 
@@ -40,17 +45,27 @@ db_filename = 'sqlite:///' + os.getcwd() + '/data/db.sqlite'
 app.config['SQLALCHEMY_DATABASE_URI'] = db_filename
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['JWT_SECRET_KEY'] = os.environ['SECRET_KEY']
 
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+    serialize_only = ('id', 'name', 'email')
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(15))
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(256))
     birth_date = db.Column(db.String(10))
+
+
+def authenticate(email, password):
+    user = User.query.filter_by(email=email).first()
+    if check_password_hash(user.password, password):
+        return user
 
 
 @app.route('/register', methods=['POST'])
@@ -83,16 +98,34 @@ def login():
     if form.validate() == False:
         return jsonify({'error': True, 'message': 'Validation failed', 'validations': list(form.errors.items())}), 400
 
-    user = User.query.filter_by(email=form.email.data).first()
+    user = authenticate(form.email.data, form.password.data)
     if not user:
         return jsonify({'error': True, 'message': 'Bad credentials, email or password'}), 401
 
-    if not check_password_hash(user.password, form.password.data):
-        return jsonify({'error': True, 'message': 'Bad credentials, email or password'}), 401
+    return jsonify({
+        'id': user.id,
+        'access_token': create_access_token(identity=user.id),
+    }), 200
 
-    return jsonify({'id': user.id}), 200
+
+@app.route('/me')
+@jwt_required
+def me():
+    user_id = get_jwt_identity()
+    return jsonify(User.query.get(user_id).to_dict())
 
 
-if __name__ == 'main':
+@app.route('/guests')
+@jwt_required
+def protected():
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
+
+
+@app.before_first_request
+def create_tables():
     db.create_all()
+
+
+if __name__ == '__main__':
     app.run(debug=True)
