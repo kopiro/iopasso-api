3bff62dae7693265bbb377e623c8c9af86908aeb
Flavio De Stefano <destefano.flavio@gmail.com>
2021-02-03 19:37:40 +0100

    adding CRUD events

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash
 from forms import LoginForm, RegisterForm, EventForm
 from common import app, db
 from models import User, Event
-from flask import jsonify, request
+from flask import json, jsonify, request
 from werkzeug.security import check_password_hash
 from dotenv import load_dotenv
 
@@ -14,6 +14,8 @@ load_dotenv()
 
 def authenticate(email, password):
     user = User.query.filter_by(email=email).first()
+    if not user:
+        return None
     if check_password_hash(user.password, password):
         return user
 
@@ -54,7 +56,7 @@ def login():
 
     return jsonify({
         'access_token': create_access_token(identity=user.id),
-        'user': user.to_dict(),
+        'id': user.id
     }), 200
 
 
@@ -79,15 +81,70 @@ def events_create():
     if form.validate() == False:
         return jsonify({'error': True, 'message': "Validation failed", 'validations': list(form.errors.items())}), 400
 
+    user_id = get_jwt_identity()
+
     event = Event(
         name=form.name.data,
         address=form.address.data,
+        owner_id=user_id
     )
 
     db.session.add(event)
     db.session.commit()
 
-    return jsonify({'success': True, 'event': event.to_dict()}), 200
+    return jsonify({
+        'success': True,
+        'id': event.id
+    }), 200
+
+
+@app.route('/events', methods=['GET'])
+@jwt_required
+def events_collection_get():
+    page = request.args.get('page', 1, type=int)
+    limit = request.args.get('limit', 10, type=int)
+    total = Event.query.count()
+    events = Event.query.offset(page*limit).limit(limit).all()
+    return jsonify({
+        'limit': limit,
+        'page': page,
+        'total': total,
+        'data': [event.to_dict(only=('id', 'name', 'owner_id')) for event in events]
+    })
+
+
+@app.route('/events/<id>', methods=['GET'])
+@jwt_required
+def event_model_get(id):
+    event = Event.query.get(id)
+    if not event:
+        return jsonify({'error': True, 'message': 'Not found'}), 404
+    return jsonify(event.to_dict())
+
+
+@app.route('/events/<id>', methods=['PUT'])
+@jwt_required
+def event_model_modify(id):
+    form = EventForm(request.form)
+    if form.validate() == False:
+        return jsonify({'error': True, 'message': "Validation failed", 'validations': list(form.errors.items())}), 400
+
+    if request.form.get('id'):
+        return jsonify({'error': True, 'message': "ID can't change"}), 400
+
+    num_rows_updated = Event.query.filter_by(id=id).update(request.form)
+    db.session.commit()
+
+    return jsonify(num_rows_updated >= 1)
+
+
+@app.route('/events/<id>', methods=['DELETE'])
+@jwt_required
+def event_model_delete(id):
+    num_rows_updated = Event.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    return jsonify(num_rows_updated >= 1)
 
 
 @app.before_first_request
--- src/models.py
+++ src/models.py
@@ -15,9 +15,11 @@ class User(db.Model, SerializerMixin):
 
 class Event(db.Model, SerializerMixin):
     __tablename__ = 'events'
-    serialize_only = ('id', 'name', 'address')
+    serialize_only = ('id', 'name', 'address', 'owner_id', 'owner')
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     address = db.Column(db.String(250))
-    # guests = db.Column(db.String(250))
+    owner_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    owner = db.relationship(
+        "User", backref=db.backref("users", uselist=False))
